f3265fe429d82e80110eaa526e100065183d8b35
AlexBogdanov <alex.e.bogdanov@gmail.com>
2018-10-03 22:38:55 +0300

    Tasks added.

--- JavascriptFundamentals/04.ArraysAndMatrices/Exercises/04RotateArray.js
+++ JavascriptFundamentals/04.ArraysAndMatrices/Exercises/04RotateArray.js
@@ -3,15 +3,6 @@ function rotateArr(data) {
     data.pop();
 
     for (let i = 0; i < rotations % data/length; i++) {
-        // let tmp = [];
-        // tmp.push(data[data.length-1]);
-
-        // for (let i = 0; i < data.length - 1; i++) {
-        //     tmp.push(data[i]);
-        // }
-
-        // data = tmp.slice(0);
-
         data.unshift(data.pop());
     }
 
